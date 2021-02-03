b6106b1ce6fa440d2bde957097397d2d147ad290
Catalina Radu <sea.cat.cata@gmail.com>
2021-02-03 02:58:07 +0000

    + location permission logic + icon

--- app/src/main/java/ro/seacat/weatherapp/ui/BaseActivity.java
+++ app/src/main/java/ro/seacat/weatherapp/ui/BaseActivity.java
@@ -1,17 +1,82 @@
 package ro.seacat.weatherapp.ui;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Settings;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import ro.seacat.weatherapp.R;
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+
+  protected final static int LOCATION_PERMISSION = 321;
+  private static final String PACKAGE = "package";
 
   private Toast toast;
-  private Snackbar snackbar;
+
+  protected void checkLocationPermission() {
+    if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+      requestLocationPermission();
+    else
+      locationPermissionGranted();
+  }
+
+  public void requestLocationPermission() {
+    if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+      createLocationPermissionRequest();
+      return;
+    }
+
+    UIHelper.createDialog(this,
+        R.string.location_permission_title, R.string.location_permission_rationale,
+        (dialog, which) -> createLocationPermissionRequest(), (dialog, which) -> dialog.dismiss());
+  }
+
+  private void createLocationPermissionRequest() {
+    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION);
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    if (requestCode != LOCATION_PERMISSION)
+      return;
+
+    if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+      locationPermissionGranted();
+    else
+      locationPermissionDenied();
+  }
+
+  protected void locationPermissionGranted() {
+    showToast("permission granted");
+  }
+
+  protected void locationPermissionDenied() {
+    ViewGroup rootLayout = (ViewGroup) ((ViewGroup) (findViewById(android.R.id.content))).getChildAt(0);
+    if (!(rootLayout instanceof CoordinatorLayout)) {
+      showToast(R.string.error_location_permission_needed);
+      return;
+    }
+
+    Snackbar.make(rootLayout, R.string.error_location_permission_needed, Snackbar.LENGTH_INDEFINITE)
+        .setAction(R.string.settings, v -> {
+          Intent settingsIntent = new Intent();
+          settingsIntent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+          settingsIntent.setData(Uri.fromParts(PACKAGE, getPackageName(), null));
+          startActivity(settingsIntent);
+        }).show();
+  }
 
   protected void showToast(int stringId) {
     if (toast != null)
@@ -28,18 +93,14 @@ public class BaseActivity extends AppCompatActivity {
   }
 
   protected void showSnackBar(View view, int stringId) {
-    if (snackbar != null)
-      snackbar.dismiss();
-    snackbar = Snackbar.make(view, stringId, Snackbar.LENGTH_INDEFINITE);
-    snackbar.setAction(R.string.dismiss, v -> snackbar.dismiss());
-    snackbar.show();
+    Snackbar snackbar = Snackbar.make(view, stringId, Snackbar.LENGTH_INDEFINITE);
+    snackbar.setAction(R.string.dismiss, v -> snackbar.dismiss())
+        .show();
   }
 
   protected void showSnackBar(View view, String string) {
-    if (snackbar != null)
-      snackbar.dismiss();
-    snackbar = Snackbar.make(view, string, Snackbar.LENGTH_INDEFINITE);
-    snackbar.setAction(R.string.dismiss, v -> snackbar.dismiss());
-    snackbar.show();
+    Snackbar snackbar = Snackbar.make(view, string, Snackbar.LENGTH_INDEFINITE);
+    snackbar.setAction(R.string.dismiss, v -> snackbar.dismiss())
+        .show();
   }
 }
---
+++ app/src/main/java/ro/seacat/weatherapp/ui/UIHelper.java
@@ -0,0 +1,21 @@
+package ro.seacat.weatherapp.ui;
+
+import android.content.Context;
+import android.content.DialogInterface;
+
+import androidx.appcompat.app.AlertDialog;
+import ro.seacat.weatherapp.R;
+
+public class UIHelper {
+
+  public static void createDialog(Context context, int titleResource, int messageResource, DialogInterface.OnClickListener positiveActionListener, DialogInterface.OnClickListener negativeButtonListener) {
+    new AlertDialog.Builder(context)
+        .setTitle(titleResource)
+        .setMessage(messageResource)
+        .setPositiveButton(R.string.ok, positiveActionListener)
+        .setNegativeButton(R.string.cancel, negativeButtonListener)
+        .create()
+        .show();
+  }
+}
+
